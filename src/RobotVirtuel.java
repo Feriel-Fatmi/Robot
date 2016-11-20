0b9a19ddce1e9dcec19466e5ba341a8fd0790444
Feriel9 <ferielmeriem95@gmail.com>
2016-11-20 19:32:28 +0100

    Update RobotVirtuel.java

--- src/RobotVirtuel.java
+++ src/RobotVirtuel.java
@@ -1,78 +1,80 @@
 /**
  * Created by Fatmi on 20/11/2016.
  */
-public class RobotVirtuel{
+public class Robot{
         private String nom;
         private int x;
         private int y;
         private String direction;
+ public Robot (String nom){
+        this.nom=nom;
+        this.direction="Est";
+        this.x=0;
+        this.y=0;
+    }
+    public Robot (String nom,int x,int y,String direction){
+        this.nom=nom;
+        this.direction=direction;
+        this.x=x;
+        this.y=y;
+    }
 
-        public RobotVirtuel(int x, int y, String nom, String direction) {
-            this.x = x;
-            this.y = y;
-            this.nom = nom;
-            this.direction = direction;
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void avance(){
+        switch(direction){
+            case "Est":
+                x++;
+                break;
+            case"Sud":
+                y--;
+                break;
+            case "Nord":
+                y++;
+                break;
+            case "Ouest":
+                x--;
+                break;
         }
-
-        public RobotVirtuel(String nom) {
-            this.x = 0;
-            this.y = 0;
-            this.nom = nom;
-            this.direction = "Est";
-        }
-
-        public void avance() {
-            switch (direction) {
-                case "Est":
-                    x++;
-                case "Nord":
-                    y++;
-                case "Sud":
-                    y--;
-                case "Ouest":
-                    x--;
-            }
-        }
-
-        public void droite() {
-            if (direction == "Nord")
-                direction = "Est";
-            if (direction == "Est")
-                direction = "Sud";
-            if (direction == "Sud")
-                direction = "Ouest";
-            if (direction == "Ouest")
-                direction = "Nord";
-        }
-
-        public void affiche() {
-            System.out.println("le robot est " + nom + " sa direction est " + direction+" sa position est "+( +x+" , "+y));
-
-        }
-
-        public String getDirection() {
-            return direction;
-        }
-
-        public void setDirection(String direction) {
-            this.direction = direction;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-
+    }
+    public void droite(){
+        switch(direction){
+            case "Nord":
+                direction="Est";
+            case "Est":
+                direction="Sud";
+            case "Sud":
+                direction="Ouest";
+            case "Ouest":
+                direction="Nord";
+        }}
+        public void affiche(){
+            System.out.println("le robot est "+ nom +" sa position est "+ "(" + x +","+ y+")"+" sa direction est "+direction);
+    }
 }
+      
